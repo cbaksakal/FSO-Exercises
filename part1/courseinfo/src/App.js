6bf2375fb4d7a2a7fccac94baeccc91155fe7e7a
cengiz <aksakalcengiz@hotmail.com>
2021-07-02 15:42:00 +0300

    courseinfo exercise 1.1 is added

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,5 +1,29 @@
 import React, {useState} from "react";
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course} </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.part1} {props.ex1} </p>
+      <p>{props.part2} {props.ex2} </p>
+      <p>{props.part3} {props.ex3} </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3} </p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -11,17 +35,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course} </h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3} </p>
+      <Header course={course} />
+      <Content part1={part1} ex1={exercises1}
+      part2={part2} ex2={exercises2}
+      part3={part3} ex3={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
     </div>
   )
 }
